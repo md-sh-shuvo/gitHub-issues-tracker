fd3106a875a59397f550f8e7459c89115b9bd1ef
md-sh-shuvo <shuvo.avail@gmail.com>
2026-03-09 15:00:32 +0600

    issues count functionality added for all tabs

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,24 @@
+let cards = [];
+const issuesCount = document.getElementById("issues-count");
+
 const fetchData = async () => {
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
   const data = await res.json();
-  const cards = data.data;
+  cards = data.data;
   showCards(cards);
 };
 
 fetchData();
+const cardContainer = document.getElementById("card-container");
 const showCards = (cards) => {
   console.log(cards);
-  const cardContainer = document.getElementById("card-container");
 
+  issuesCount.innerText = cards.length;
   cardContainer.innerHTML = cards
     .map((card) => {
-        const formateDate = new Date(card.createdAt).toLocaleDateString("en-US")
+      const formateDate = new Date(card.createdAt).toLocaleDateString("en-US");
       return `
         <div
                     id="card"
@@ -34,10 +38,12 @@ const showCards = (cards) => {
 
                             <span
                                 class="px-4 py-1 text-sm font-semibold ${
-                                    card.priority === "high" ? 
-                                        "text-red-500 bg-red-100" :
-                                            card.priority === "medium" ?
-                                               "text-yellow-500 bg-orange-100" : "text-gray-500 bg-gray-200"} rounded-full"
+                                  card.priority === "high"
+                                    ? "text-red-500 bg-red-100"
+                                    : card.priority === "medium"
+                                      ? "text-yellow-500 bg-orange-100"
+                                      : "text-gray-500 bg-gray-200"
+                                } rounded-full"
                             >
                                 ${card.priority.toUpperCase()}
                             </span>
@@ -97,6 +103,260 @@ const handleTab = async (e) => {
   });
 
   e.target.classList.add("active");
+
+  if (e.target.id === "open") {
+    issuesCount.innerText = cards.filter(
+      (card) => card.status === "open",
+    ).length;
+
+    cardContainer.innerHTML = cards
+      .map((card) => {
+        if (card.status === "open") {
+          const formateDate = new Date(card.createdAt).toLocaleDateString(
+            "en-US",
+          );
+          return `
+        <div
+                    id="card"
+                    class="card max-w-sm bg-white rounded-xl shadow-md border border-gray-200 overflow-hidden border-t-4  ${card.status === "open" ? "border-t-green-500" : "border-t-purple-500"} justify-between"
+                >
+                    <div class="p-5">
+                        <div class="flex justify-between items-start mb-3">
+                            <div
+                                class="w-7 h-7 flex items-center justify-center bg-green-200 rounded-full"
+                            >
+                                <img
+                                    class="w-4 h-4"
+                                    src="assets/${card.status === "open" ? "openStatus" : "closedStatus"}.svg"
+                                    alt=""
+                                />
+                            </div>
+
+                            <span
+                                class="px-4 py-1 text-sm font-semibold ${
+                                  card.priority === "high"
+                                    ? "text-red-500 bg-red-100"
+                                    : card.priority === "medium"
+                                      ? "text-yellow-500 bg-orange-100"
+                                      : "text-gray-500 bg-gray-200"
+                                } rounded-full"
+                            >
+                                ${card.priority.toUpperCase()}
+                            </span>
+                        </div>
+
+                        <h2 class="text-sm font-semibold text-[#1F2937] mb-2">
+                            ${card.title}
+                        </h2>
+
+                        <p class="text-[#64748B] text-[12px] mb-4">
+                            ${card.description}
+                        </p>
+
+                        <div class="flex gap-2">
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#EF4444] border border-red-300 bg-[#FECACA] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/bug.svg"
+                                    alt=""
+                                />
+
+                                ${card.labels[0]}
+                            </span>
+
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#D97706] border border-orange-300 bg-[#FDE68A] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/helpWanted.svg"
+                                    alt=""
+                                />
+                                ${card.labels[1]}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div
+                        class="px-5 py-4 border-t-2 border-gray-200 text-[12px] text-[#64748B]"
+                    >
+                        <p>#${card.id} ${card.author}</p>
+                        <p>${formateDate} </p>
+                    </div>
+                </div>
+    `;
+        }
+      })
+      .join("");
+  } else if (e.target.id === "closed") {
+    issuesCount.innerText = cards.filter(
+      (card) => card.status === "closed",
+    ).length;
+    cardContainer.innerHTML = cards
+      .map((card) => {
+        if (card.status === "closed") {
+          const formateDate = new Date(card.createdAt).toLocaleDateString(
+            "en-US",
+          );
+          return `
+        <div
+                    id="card"
+                    class="card max-w-sm bg-white rounded-xl shadow-md border border-gray-200 overflow-hidden border-t-4  ${card.status === "open" ? "border-t-green-500" : "border-t-purple-500"} justify-between"
+                >
+                    <div class="p-5">
+                        <div class="flex justify-between items-start mb-3">
+                            <div
+                                class="w-7 h-7 flex items-center justify-center bg-green-200 rounded-full"
+                            >
+                                <img
+                                    class="w-4 h-4"
+                                    src="assets/${card.status === "open" ? "openStatus" : "closedStatus"}.svg"
+                                    alt=""
+                                />
+                            </div>
+
+                            <span
+                                class="px-4 py-1 text-sm font-semibold ${
+                                  card.priority === "high"
+                                    ? "text-red-500 bg-red-100"
+                                    : card.priority === "medium"
+                                      ? "text-yellow-500 bg-orange-100"
+                                      : "text-gray-500 bg-gray-200"
+                                } rounded-full"
+                            >
+                                ${card.priority.toUpperCase()}
+                            </span>
+                        </div>
+
+                        <h2 class="text-sm font-semibold text-[#1F2937] mb-2">
+                            ${card.title}
+                        </h2>
+
+                        <p class="text-[#64748B] text-[12px] mb-4">
+                            ${card.description}
+                        </p>
+
+                        <div class="flex gap-2">
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#EF4444] border border-red-300 bg-[#FECACA] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/bug.svg"
+                                    alt=""
+                                />
+
+                                ${card.labels[0]}
+                            </span>
+
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#D97706] border border-orange-300 bg-[#FDE68A] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/helpWanted.svg"
+                                    alt=""
+                                />
+                                ${card.labels[1]}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div
+                        class="px-5 py-4 border-t-2 border-gray-200 text-[12px] text-[#64748B]"
+                    >
+                        <p>#${card.id} ${card.author}</p>
+                        <p>${formateDate} </p>
+                    </div>
+                </div>
+    `;
+        }
+      })
+      .join("");
+  } else {
+    issuesCount.innerText = cards.length;
+    cardContainer.innerHTML = cards
+      .map((card) => {
+        const formateDate = new Date(card.createdAt).toLocaleDateString(
+          "en-US",
+        );
+        return `
+        <div
+                    id="card"
+                    class="card max-w-sm bg-white rounded-xl shadow-md border border-gray-200 overflow-hidden border-t-4  ${card.status === "open" ? "border-t-green-500" : "border-t-purple-500"} justify-between"
+                >
+                    <div class="p-5">
+                        <div class="flex justify-between items-start mb-3">
+                            <div
+                                class="w-7 h-7 flex items-center justify-center bg-green-200 rounded-full"
+                            >
+                                <img
+                                    class="w-4 h-4"
+                                    src="assets/${card.status === "open" ? "openStatus" : "closedStatus"}.svg"
+                                    alt=""
+                                />
+                            </div>
+
+                            <span
+                                class="px-4 py-1 text-sm font-semibold ${
+                                  card.priority === "high"
+                                    ? "text-red-500 bg-red-100"
+                                    : card.priority === "medium"
+                                      ? "text-yellow-500 bg-orange-100"
+                                      : "text-gray-500 bg-gray-200"
+                                } rounded-full"
+                            >
+                                ${card.priority.toUpperCase()}
+                            </span>
+                        </div>
+
+                        <h2 class="text-sm font-semibold text-[#1F2937] mb-2">
+                            ${card.title}
+                        </h2>
+
+                        <p class="text-[#64748B] text-[12px] mb-4">
+                            ${card.description}
+                        </p>
+
+                        <div class="flex gap-2">
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#EF4444] border border-red-300 bg-[#FECACA] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/bug.svg"
+                                    alt=""
+                                />
+
+                                ${card.labels[0]}
+                            </span>
+
+                            <span
+                                class="flex items-center gap-1 px-3 py-1 text-[12px] font-medium text-[#D97706] border border-orange-300 bg-[#FDE68A] rounded-full"
+                            >
+                                <img
+                                    class="w-3 h-3 mt-[0.8px]"
+                                    src="assets/helpWanted.svg"
+                                    alt=""
+                                />
+                                ${card.labels[1]}
+                            </span>
+                        </div>
+                    </div>
+
+                    <div
+                        class="px-5 py-4 border-t-2 border-gray-200 text-[12px] text-[#64748B]"
+                    >
+                        <p>#${card.id} ${card.author}</p>
+                        <p>${formateDate} </p>
+                    </div>
+                </div>
+    `;
+      })
+      .join("");
+  }
 
   // if(e.target.id === "all") console.log("all")
   // else if(e.target.id === "open") console.log("open")
